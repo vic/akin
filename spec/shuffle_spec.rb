513bbef4f159a9452c6c6e40cd42f724594a004a
Victor Hugo Borja <vic.borja@gmail.com>
2011-09-20 00:06:27 -0500

    Fix specs for op-shuffling on rb19

--- spec/shuffle_spec.rb
+++ spec/shuffle_spec.rb
@@ -6,84 +6,109 @@ describe 'Akin operator shuffle' do
 
   describe 'basic math opers' do
     it 'associates correctly + and *' do
-      n('a + b * c - d').should ==
-        [:chain,
-         [:chain, [:name, "a"], [:oper, "+"],
-          [:send, nil, 
-           [:chain, [:name, "b"], [:oper, "*"], [:send, nil, [:name, "c"]]]]],
+      n('a + b * c - d').should == 
+        [:chain, 
+         [:name, "a"], 
+         [:oper, "+"],
+         [:send, nil, 
+          [:chain, [:name, "b"], [:oper, "*"], [:send, nil, [:name, "c"]]]],
          [:oper, "-"],
          [:send, nil, [:name, "d"]]]
+    end
+
+    it 'should not shuffle already shuffled operators' do
+      sexp = [:chain, 
+              [:name, "a"], 
+              [:oper, "+"],
+              [:send, nil, 
+               [:chain, [:name, "b"], [:oper, "*"], [:send, nil, [:name, "c"]]]],
+              [:oper, "-"],
+              [:send, nil, [:name, "d"]]]
+      n = c('a + b * c - d')
+      s = shuffler.shuffle(n)
+      s.sexp.should == sexp
+      m = shuffler.shuffle(s)
+      m.sexp.should == sexp
     end
   end
 
 
-=begin
+
   describe 'assignment' do
     it 'takes lhs and rhs' do
       n('a = b').should ==
-        [:act, "=", nil, [:name, "a"], [:name, "b"]]
+        [:chain, [:oper, "="], 
+         [:send, nil, [:name, "a"], [:name, "b"]]]
     end
 
     it 'has higher precedence than +' do
-      n('a = b + c').should ==
-        [:act, "=", nil,
-         [:name, "a"],
-         [:chain, [:name, "b"],
-          [:act, "+", nil, [:name, "c"]]]]
+      [:chain, [:oper, "="],
+       [:send, nil, [:name, "a"],
+        [:chain, [:name, "b"], [:oper, "+"],
+         [:send, nil, [:name, "c"]]]]]
     end
 
     it 'has higher precedence than both + and *' do
       n('a * d = b + c').should ==
-        [:act, "=", nil,
-         [:chain, [:name, "a"],
-          [:act, "*", nil, [:name, "d"]]],
-         [:chain, [:name, "b"],
-          [:act, "+", nil, [:name, "c"]]]]
+        [:chain, [:oper, "="],
+         [:send, nil, 
+          [:chain, [:name, "a"], [:oper, "*"], [:send, nil, [:name, "d"]]],
+          [:chain, [:name, "b"], [:oper, "+"], [:send, nil, [:name, "c"]]]]]
     end
 
     it 'is right associative' do
       n('a = b = c').should ==
-        [:act, "=", nil, [:name, "a"], [:act, "=", nil, [:name, "b"], [:name, "c"]]]
+        [:chain, [:oper, "="],
+         [:send, nil,
+          [:name, "a"], [:chain, [:oper, "="], 
+                         [:send, nil, [:name, "b"], [:name, "c"]]]]]
     end
 
     it 'is parsed correctly with &&' do
       n('a && b = c').should ==
         [:chain, [:name, "a"],
-         [:act, "&&", nil, [:act, "=", nil, [:name, "b"], [:name, "c"]]]]
+         [:oper, "&&"],
+         [:send, nil, 
+          [:chain, [:oper, "="], [:send, nil, [:name, "b"], [:name, "c"]]]]]
     end
 
     it 'is parsed correctly with and' do
       n('a and b = c').should ==
         [:chain, [:name, "a"],
-         [:act, "and", nil, [:act, "=", nil, [:name, "b"], [:name, "c"]]]]
+         [:oper, "and"],
+         [:send, nil, 
+          [:chain, [:oper, "="], [:send, nil, [:name, "b"], [:name, "c"]]]]]
     end
 
     it 'shuffles correctly with or' do
       n('a = b = c or d').should ==
-        [:chain, [:act, "=", nil,
-                  [:name, "a"],
-                  [:act, "=", nil, [:name, "b"], [:name, "c"]]],
-         [:act, "or", nil, [:name, "d"]]]
-    end
+        [:chain, 
+         [:oper, "="], 
+         [:send, nil, [:name, "a"], 
+          [:chain, [:oper, "="], 
+           [:send, nil, [:name, "b"], [:name, "c"]]]], 
+         [:oper, "or"], [:send, nil, [:name, "d"]]]
+     end
   end
 
   describe 'unary negation' do
     it 'binds to right' do
-      n('!b').should ==
-        [:act, "!", nil, [:name, "b"]]
+      n('!b').should == [:chain, [:oper, "!"], [:send, nil, [:name, "b"]]]
     end
 
     it 'binds chain to right' do
       n('!b c d').should ==
-        [:act, "!", nil,
-         [:chain, [:name, "b"], [:name, "c"], [:name, "d"]]]
+        [:chain, [:oper, "!"], [:send, nil,
+         [:chain, [:name, "b"], [:name, "c"], [:name, "d"]]]]
     end
 
     it 'binds chain to right till oper' do
       n('!b c + d').should ==
-        [:chain,
-         [:act, "!", nil, [:chain, [:name, "b"], [:name, "c"]]],
-         [:act, "+", nil, [:name, "d"]]]
+        [:chain, 
+         [:oper, "!"],
+         [:send, nil, [:chain, [:name, "b"], [:name, "c"]]],
+         [:oper, "+"],
+         [:send, nil, [:chain, [:name, "d"]]]]
     end
 
     it 'binds chain to right till end' do
@@ -108,6 +133,7 @@ describe 'Akin operator shuffle' do
            [:act, "&&", nil, [:name, "d"]]]]]
     end
   end
+=begin
 
   describe 'logical operator &&' do
     it 'associates correctly with logical opers' do
